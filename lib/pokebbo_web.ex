d88b79df43919bad3aa9acdaa65273fa491ec47f
Christian <Xxamer_@hotmail.com>
2026-08-08 19:03:54 +0100

    Added display pixel avatars, some animation and ignore when updating server responses (#3)

--- lib/pokebbo_web.ex
+++ lib/pokebbo_web.ex
@@ -17,7 +17,7 @@ defmodule PokebboWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts avatars backgrounds images favicon.ico robots.txt)
 
   def router do
     quote do
